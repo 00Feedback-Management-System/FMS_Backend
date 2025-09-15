1a6285f6c205fb609357f46ecf591970107bf84d
DESKTOP-1G1A5EV\Suhas Patil <suhasmdpatil@gmail.com>
2025-09-15 09:34:16 +0530

    added api logic for fetch submited feedback details

---
+++ Feedback_System/DTO/SubmittedFeedbackDetailsDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Feedback_System.DTO
+{
+    public class SubmittedFeedbackDetailsDto
+    {
+        public int FeedbackGroupId { get; set; }
+        public string FeedbackTypeName { get; set; }
+        public string CourseName { get; set; }
+        public string ModuleName { get; set; }
+        public string StaffName { get; set; }
+        public int Session { get; set; }
+        public List<FeedbackAnswerDto> Answers { get; set; }
+    }
+}
