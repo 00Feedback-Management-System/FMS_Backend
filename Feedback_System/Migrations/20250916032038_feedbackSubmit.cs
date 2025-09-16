7e17f99fd5c984e068a5b701e3c865d8a3c122d4
Saurabh9172 <9172169879misal@gmail.com>
2025-09-16 09:00:02 +0530

    add feedbackgroupid in feedbacksubmit

---
+++ Feedback_System/Migrations/20250916032038_feedbackSubmit.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Feedback_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class feedbackSubmit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "feedback_group_id",
+                table: "FeedbackSubmits",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FeedbackSubmits_feedback_group_id",
+                table: "FeedbackSubmits",
+                column: "feedback_group_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FeedbackSubmits_FeedbackGroup_feedback_group_id",
+                table: "FeedbackSubmits",
+                column: "feedback_group_id",
+                principalTable: "FeedbackGroup",
+                principalColumn: "FeedbackGroupId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_FeedbackSubmits_FeedbackGroup_feedback_group_id",
+                table: "FeedbackSubmits");
+
+            migrationBuilder.DropIndex(
+                name: "IX_FeedbackSubmits_feedback_group_id",
+                table: "FeedbackSubmits");
+
+            migrationBuilder.DropColumn(
+                name: "feedback_group_id",
+                table: "FeedbackSubmits");
+        }
+    }
+}
