5864571ba5cdcada22a326267f640bb1be7d5c64
teju1809 <tejaswinijadhav189@gmail.com>
2025-09-17 13:07:39 +0530

    update coursewise report api

---
+++ Feedback_System/DTO/CoursewiseReportDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Feedback_System.DTO
+{
+    public class CoursewiseReportDto
+    {
+
+        public string CourseName { get; set; }
+        public string ModuleName { get; set; }
+        public List<string> FeedbackTypeNames { get; set; } = new List<string>();
+    }
+}
