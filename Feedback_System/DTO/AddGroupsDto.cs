2e48daf3e71c9c7a5c395bc2f6155ba2360dfefc
Saurabh9172 <9172169879misal@gmail.com>
2025-09-22 09:57:21 +0530

    Groupcount removed from Group

---
+++ Feedback_System/DTO/AddGroupsDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Feedback_System.DTO
+{
+    public class AddGroupsDto
+    {
+        public int course_id { get; set; }
+        public List<string> groups { get; set; }
+    }
+
+}
---
+++ Feedback_System/DTO/CourseFeedbackReportDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Feedback_System.DTO
+{
+    public class CourseFeedbackReportDto
+    {
+        public DateTime Date { get; set; }        // Max EndDate
+        public string CourseName { get; set; }
+        public string FeedbackTypeName { get; set; }
+        public string Groups { get; set; }        // Single / Multiple
+        public int Sessions { get; set; }         // Sum of sessions
+        public double Rating { get; set; }        // Avg rating
+    }
+}
